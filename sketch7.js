1626ae8eab849a27732211a2aef110bf988fe5fb
Kristin Hudson <Kristin@7c-d1-c3-70-af-86.wlan.lsu.edu>
2016-10-06 18:32:05 -0500

    okay

--- sketch7.js
+++ sketch7.js
@@ -1,38 +1,71 @@
 var fran;
 var song;
-var r = 0;
-var x = 0;
-var y = 0;
-var xs = 0;
-var ys = 0;
+var x1;
+var y1;
+var x2;
+var y2;
 var lxeye = 0;
 var rxeye = 0;
 var lyeye = 0;
 var ryeye = 0;
+var time1 = 1000;
+var time2 = 2800;
+var time3 = 4800;
+var time4 = 6300;
+var time5 = 8200;
+var time6 = 10000;
+var time7 = 13500;
+var time8 = 14100;
+var time9 = 14700;
 
 function preload() {
-  fran = loadImage("fran.png");
+  fran = loadImage("franback.png");
   song = loadSound("delerueb.mp3");
 }
 
 function setup() {
-  createCanvas (800, 800);
-  s = 2;
-  r = 100;
-  zx = random(0, 800);
-  zy = random(0, 800);
+  createCanvas (2000, 800);
   song.loop();
-
-  lxeye = x - 10;
-  rxeye = x - 10;
-  lyeye = y - 600;
-  ryeye = y - 600;
+  x2 = 200;
+  y2 = 500;
 
 }
 
 function draw() {
-  x = zx + s;
-  y = zy + s;
+  var currentTime = millis();
+  if (currentTime > time9) {
+    x1 = 20;
+    y1 = 30;
+    x2 = 200;
+    y2 = 500;
+  } else if (currentTime > time8) {
+    x1 = 50;
+    y1 = 70;
+  } else if (currentTime > time7) {
+    x1 = 300;
+    y1 = 400;
+  } else if (currentTime > time6) {
+    x1 = 600;
+    y1 = 70;
+  } else if (currentTime > time5) {
+    x1 = 200;
+    y1 = 500;
+  } else if (currentTime > time4) {
+    x1 = 20;
+    y1 = 300;
+  } else if (currentTime > time3) {
+    x1 = 30;
+    y1 = 100;
+  } else if (currentTime > time2) {
+    x1 = 70;
+    y1 = 500;
+  } else if (currentTime > time1) {
+    x1 = 50;
+    y1 = 400;
+  }
 
-  ellipse(x, y, r);
+image(fran, x2, y2, 300, 250);
+noStroke();
+fill(random(0, 255), random(0, 255), random(0, 255), 90);
+ellipse(x1, y1, random(10, 100));
 }
