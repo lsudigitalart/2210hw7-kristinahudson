4f3d4a8a9d426fc0d4fed25f840a62ff32386d18
Kristin Hudson <Kristin@Kristins-MacBook-Pro-3.local>
2016-10-10 23:40:32 -0500

    tonight's hw

--- sketch7.js
+++ sketch7.js
@@ -1,71 +1,92 @@
-var fran;
 var song;
-var x1;
-var y1;
-var x2;
-var y2;
-var lxeye = 0;
-var rxeye = 0;
-var lyeye = 0;
-var ryeye = 0;
-var time1 = 1000;
-var time2 = 2800;
-var time3 = 4800;
-var time4 = 6300;
-var time5 = 8200;
-var time6 = 10000;
-var time7 = 13500;
-var time8 = 14100;
-var time9 = 14700;
+var x = 600
+var y = 400
+var x2 = 637
+var y2 = 80
+var w = 5
+var h = 5
+var w2 = 0
+var h2 = 0
+var x3 = 637
+var y3 = 235
+var w3 = 0
+var h3 = 0
+
+
+
+
 
 function preload() {
-  fran = loadImage("franback.png");
-  song = loadSound("delerueb.mp3");
+  song = loadSound("felix.mp3");
 }
 
 function setup() {
   createCanvas (2000, 800);
   song.loop();
-  x2 = 200;
-  y2 = 500;
+  angleMode(DEGREES);
 
 }
 
 function draw() {
-  var currentTime = millis();
-  if (currentTime > time9) {
-    x1 = 20;
-    y1 = 30;
-    x2 = 200;
-    y2 = 500;
-  } else if (currentTime > time8) {
-    x1 = 50;
-    y1 = 70;
-  } else if (currentTime > time7) {
-    x1 = 300;
-    y1 = 400;
-  } else if (currentTime > time6) {
-    x1 = 600;
-    y1 = 70;
-  } else if (currentTime > time5) {
-    x1 = 200;
-    y1 = 500;
-  } else if (currentTime > time4) {
-    x1 = 20;
-    y1 = 300;
-  } else if (currentTime > time3) {
-    x1 = 30;
-    y1 = 100;
-  } else if (currentTime > time2) {
-    x1 = 70;
-    y1 = 500;
-  } else if (currentTime > time1) {
-    x1 = 50;
-    y1 = 400;
+  background(255);
+  var ct = millis();
+  if (( ct > 1000) && (ct < 2999)) {
+    w *= 1.06;
+    h *= 1.06;
+  }
+  if (( ct > 3000) && (ct < 3699)) {
+  w *= 99/100;
+  h *= 99/100;
+}
+if (( ct > 3700) && (ct < 3999)) {
+  w *= 1.03;
+  h *= 1.03;
+}
+if((ct > 4000) && (ct < 4499)) {
+  w *= 99/100;
+  h *= 99/100;
+}
+if ((ct > 4500) && (ct < 5599)) {
+  x -= 4;
+}
+if ((ct > 5600) && (ct < 6199)) {
+  w *= .98;
+  h *= .98;
+}
+if ((ct > 6200) && (ct < 7199)) {
+  x+=5;
+  y-=5;
+  w*=.99;
+  h*=.99;
+}
+if ((ct > 7200) && (ct < 8299)) {
+  y2 += 2;
+  w2 = 30;
+  h2 = 30;
+}
+if ((ct > 8300) && (ct < 8599)) {
+  y3 += 1.5;
+  w3 = 20;
+  h3 = 20;
+}
+if ((ct > 8600) && (ct < 9999)) {
+  x -= 2.5;
+  y += 2.5;
+  y2 += 1.5;
+  x3 += 1;
+  y3 += 1;
+}
+if ((ct > 10000) && (ct < 17999)) {
+  x += 2;
+  x2 += 2;
+  x3 += 2;
   }
 
-image(fran, x2, y2, 300, 250);
-noStroke();
-fill(random(0, 255), random(0, 255), random(0, 255), 90);
-ellipse(x1, y1, random(10, 100));
+  noStroke();
+  fill(100, 200, 250);
+  ellipse(x, y, w, h);
+  ellipse(x2, y2, w2, h2);
+  ellipse(x3, y3, w3, h3);
+
+
 }
